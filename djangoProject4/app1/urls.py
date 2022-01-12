c5ac0e59dc3a78f569e64e665103cdf5a282f92b
limjungwon <aliciawill@kakao.com>
2022-01-12 12:47:31 +0900

    2022-01-22 수요일

--- djangoProject4/app1/urls.py
+++ djangoProject4/app1/urls.py
@@ -7,5 +7,11 @@ import app1.views
 
 urlpatterns = [
     path('', app1.views.index, name='index'),
+    path('list', app1.views.list, name='list'),
+    path('insert', app1.views.insert, name='insert'),
+    path('one', app1.views.one, name='one'),
+    path('delete', app1.views.delete, name='delete'),
+    path('update', app1.views.update, name='update'),
+    path('update2', app1.views.update2, name='update2'),
 ]
 
--- djangoProject4/app1/views.py
+++ djangoProject4/app1/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+from app1.models import Board
+
+
 def index(request):
     return render(request, 'app1/index.html')
+
+def list(request):
+    print('list함수 호출됨.')
+    # board에서 전체 리스트를 검색해서 가지고 온다.
+    board_list = Board.objects.order_by('-id')
+    print('db에서 가지고 온 data>> ' , board_list)
+    # 가지고 온 데이터를 dic만들어준다.
+    context = {
+        'list' : board_list
+    }
+    # 데이터를 넣어서 보내줄 template지정, 데이터를 넘겨줌.
+    return render(request, 'app1/list.html', context)
+
+def one(request):
+    pass
+
+def insert(request):
+    pass
+
+def delete(request):
+    pass
+
+def update(request):
+    pass
+
+def update2(request):
+    pass
